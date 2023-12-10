5090fac9193444cf1418fdef60ca69f4ede4a836
kashyapjagwani1 <kashyap@nishaan.info>
2023-12-10 14:02:26 -0800

    Initial Commit

---
+++ SwiftUI-Weather App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI-Weather App
+//
+//  Created by Kashyap Jagwani on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
